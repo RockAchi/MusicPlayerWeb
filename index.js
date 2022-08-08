9dd03b6f5af4d0cda5e762b92c7d55f3ec09344c
RockAchi <57226456+RockAchi@users.noreply.github.com>
2022-08-08 22:35:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var rock=new Audio("https://file-examples.com/storage/fe522079b962f100d94fb66/2017/11/file_example_MP3_5MG.mp3");
+document.querySelectorAll("button")[0].addEventListener("click", handlePlay);
+document.querySelectorAll("button")[1].addEventListener("click", handlePause);
+function handlePlay(){
+rock.play();
+}
+function handlePause(){
+rock.pause();
+}
